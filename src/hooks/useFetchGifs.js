e34bcda60b9f772dd2be34e58daa37500ad569ea
Daniel <danielmelo19971@gmail.com>
2021-03-30 17:27:53 -0500

    sección 7

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -10,12 +10,10 @@ export const useFetchGifs = (category) => {
     useEffect(() => {
         getGifs(category)
             .then(imgs => {
-                setTimeout(() => {
-                    setState({
-                        data: imgs,
-                        loading: false
-                    })
-                }, 1000);
+                setState({
+                    data: imgs,
+                    loading: false
+                })
             })
     }, [category]);
 
